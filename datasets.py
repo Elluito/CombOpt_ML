147f2e637b1c58c53fb2b5a2fc75cbde0523af63
LuisAlfredo <alfredoavendano@hotmail.com>
2022-08-15 15:17:36 +0100

    First commit

---
+++ datasets.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import glob
+
+
+def dataset_helper(type:str):
+    if type == "train":
+        # Load the problems
+
+        for file_name in glob.glob("datasets/train/*.wclq"):
+            pd.read_csv(file_name,sep=" ", skiprows=1)
+            pass
+        #Load the solutions
+        for file_name in glob.glob("datasets/train/*.sol"):
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test():
+    df = pd.read_csv("datasets/C1000-9.mtx",prefix="V",sep=" ",skiprows=2)
+
+
+
+
+
+
+if __name__ == '__main__':
+    test()
