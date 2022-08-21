07e008afea86e15304291e892445e74f2ce5495f
LuisAlfredo <alfredoavendano@hotmail.com>
2022-08-21 18:24:41 +0100

    function from .wclq to .edgelist

--- datasets.py
+++ datasets.py
@@ -2,43 +2,39 @@ import pandas as pd
 import glob
 
 
-def dataset_helper(type:str):
+def read_node_values(type):
     if type == "train":
-        # Load the problems
-
         for file_name in glob.glob("datasets/train/*.wclq"):
-            pd.read_csv(file_name,sep=" ", skiprows=1)
+            df = pd.read_csv(file_name, sep=" ", skiprows=1, names=["type", "v1", "v2", "v3"])
+            nodes = df[df["type"] == "v"]
+            nodes = edges.drop(["type", "v3"], axis=1)
+            #edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
+
             pass
-        #Load the solutions
+            # Load the solutions
         for file_name in glob.glob("datasets/train/*.sol"):
             pass
 
 
+def to_edgelist(type):
+    if type == "train":
+        # Load the problems
 
+        for file_name in glob.glob("datasets/train/*.wclq"):
+            df = pd.read_csv(file_name, sep=" ", skiprows=1, names=["type", "v1", "v2", "v3"])
+            edges = df[df["type"] == "e"]
+            edges = edges.drop(["type", "v3"], axis=1)
+            edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            pass
+        # Load the solutions
+        for file_name in glob.glob("datasets/train/*.sol"):
+            pass
 
 
 def test():
-    df = pd.read_csv("datasets/C1000-9.mtx",prefix="V",sep=" ",skiprows=2)
-
-
-
-
-
+    df = pd.read_csv("datasets/C1000-9.mtx", prefix="V", sep=" ", skiprows=2)
 
 
 if __name__ == '__main__':
-    test()
+    to_edgelist("train")
