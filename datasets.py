e3e0edcf4d514f7afbbb4225541b3dd8f937f66a
LuisAlfredo <alfredoavendano@hotmail.com>
2022-08-21 19:51:04 +0100

    converted to .edgelist files train and test files

--- datasets.py
+++ datasets.py
@@ -8,12 +8,21 @@ def read_node_values(type):
             df = pd.read_csv(file_name, sep=" ", skiprows=1, names=["type", "v1", "v2", "v3"])
             nodes = df[df["type"] == "v"]
             nodes = edges.drop(["type", "v3"], axis=1)
-            #edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
+            # edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
 
             pass
             # Load the solutions
         for file_name in glob.glob("datasets/train/*.sol"):
             pass
+
+
+def get_df_solution(type):
+    if type == "train":
+        for file_name in glob.glob("datasets/train/*.sol"):
+            df = pd.read_csv(file_name, sep=" ", skiprows=1, names=["type", "v1", "v2", "v3"])
+            edges = df[df["type"] == "e"]
+            edges = edges.drop(["type", "v3"], axis=1)
+            edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
 
 
 def to_edgelist(type):
@@ -25,16 +34,24 @@ def to_edgelist(type):
             edges = df[df["type"] == "e"]
             edges = edges.drop(["type", "v3"], axis=1)
             edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
+    else:
 
-            pass
+        # Load the graph
+
+        for file_name in glob.glob("datasets/test/*.wclq"):
+            df = pd.read_csv(file_name, sep=" ", skiprows=1, names=["type", "v1", "v2", "v3"])
+            edges = df[df["type"] == "e"]
+            edges = edges.drop(["type", "v3"], axis=1)
+            edges.to_csv(file_name.replace("wclq", "edgelist"), index=False, header=False, sep=" ")
+
         # Load the solutions
-        for file_name in glob.glob("datasets/train/*.sol"):
-            pass
 
 
 def test():
     df = pd.read_csv("datasets/C1000-9.mtx", prefix="V", sep=" ", skiprows=2)
 
 
+
 if __name__ == '__main__':
     to_edgelist("train")
+    to_edgelist("test")
